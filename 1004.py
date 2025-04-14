c037cfcc4ef22c6db9d30f4b343d7893acc72311
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2025-04-14 20:51:06 +0530

    Updated 1004

--- 1004.py
+++ 1004.py
@@ -21,3 +21,17 @@ class Solution:
             max_ones = max(max_ones,end+1-start)
 
         return max_ones
+    
+
+      '''
+         Logic :
+# Sliding window approach:
+# - Move 'end' to expand the window
+# - Count how many 0s are in the current window
+# - If 0s exceed k, shrink the window from the left by moving 'start'
+# - Keep updating the max window size where 0s ≤ k
+
+# Goal: Max consecutive 1s by flipping at most k zeroes
+# Keep window valid → at most k zeroes inside
+
+     '''
