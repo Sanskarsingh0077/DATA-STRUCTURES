d8afcc2991328c195acbaed3cd2ba4d873eec677
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2025-04-17 21:30:06 +0530

    LEETCODE #2215 UPDATED

--- 2215.py
+++ 2215.py
@@ -26,3 +26,18 @@ class Solution:
 #   - If element not in set_a, add to result2
 
 # Return both results as lists inside a list
+
+
+
+#Better and cleaner Code
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set_a = set(nums1)
+        set_b = set(nums2)
+
+            #Main Logic
+        only_in_a = list(set_a - set_b)
+        only_in_b = list(set_b - set_a)
+
+        return [only_in_a,only_in_b]
