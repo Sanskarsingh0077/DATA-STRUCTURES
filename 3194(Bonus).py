5f5efb14f503c533b846ba8447f8b9dd0c5de2ef
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2025-04-18 21:29:20 +0530

    LEETCODE #3194: O(1)

--- 3194(Bonus).py
+++ 3194(Bonus).py
@@ -14,3 +14,28 @@ class Solution:
 
 
         return min(avg_arr)
+    
+
+    '''
+    Space Complexity Improved:
+    Make changes in the original nums array brings down space from O(n)
+    to O(1). No extra array.
+
+
+    nums.sort()
+        #avg_arr=[]
+
+        i=0
+        j=len(nums)-1
+
+        while i <= j:
+            nums[i] =(nums[i]+nums[j])/2
+            #avg_arr.append(averages)
+            i+=1
+            j-=1
+
+
+        #return min(nums)
+        return min(nums[: (len(nums) + 1) // 2])
+    
+    '''
