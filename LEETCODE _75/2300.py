ea2543e2b0c6b0b6ef0543099a2c0e8f0a3a00fb
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2025-06-02 20:48:03 +0530

    LEETCODE #2300

--- LEETCODE _75/2300.py
+++ LEETCODE _75/2300.py
@@ -1,6 +1,48 @@
 class Solution:
     import math
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        #Best Solution Using Binary Search and not using any python module like bisect to get index
+
+        #Function to search minPotion index
+        def get_index_of(start,end,sorted_array,elementToSearch):
+    
+            while start <= end:
+                mid = start + (end-start)//2
+                if sorted_array[mid] >= elementToSearch:
+                    possible_index = mid
+                    end = mid-1
+                else:
+                    start = mid+1
+
+            return possible_index
+                
+        #function ends here
+        potions.sort()
+        pairs = []
+        n = len(potions)
+        m = len(spells)
+        maxPotionValue = potions[n-1]
+
+        for i in range(m):
+            spell = spells[i]
+            minPotion = math.ceil((1.0*success)/spell) # spell * potions[j] >= success ⇒ potions[j] >= success / spell
+
+            if minPotion > maxPotionValue:
+                pairs.append(0)
+                continue
+
+            index = get_index_of(0,n-1,potions,minPotion)#gets the lowest point index in potion
+
+            count = n-index 
+
+            pairs.append(count)
+
+        return pairs
+
+
+
+
+
         #Brute Force but Passing 51/57 cases goes till 10^4 Gives TLE
         '''
         pairs = []
@@ -16,8 +58,8 @@ class Solution:
         
         return pairs
         '''
-
-        #Using Binary Search
+        '''
+        #Using Binary Search with using bisect module to get index of minPotion
 
         potions.sort()
         pairs = []
@@ -41,6 +83,7 @@ class Solution:
             pairs.append(count)
 
         return pairs
+        '''
 
         '''
         Approach:
@@ -50,7 +93,7 @@ class Solution:
                 •	Calculate the minimum potion value needed: minPotion = ceil(success / spell)
                 •	If minPotion is greater than the maximum potion value, append 0 (no successful pairs).
                 •	Otherwise, use binary search (bisect_left) to find the first potion ≥ minPotion.
-                •	`Count how many potions from that index to the end → these are successful.
+                •	Count how many potions from that index to the end → these are successful.
             3.	Append the count for each spell to the result list.
             4.	Return the result list.
 
@@ -64,8 +107,6 @@ class Solution:
             •	O(m) for the result list storing counts per spell.
             Total Space Complexity: O(m) (output list only; rest is in-place)
         '''
-             
-        
 
 
 
