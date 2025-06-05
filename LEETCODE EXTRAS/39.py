bddf01b5910c6cd187889f58e3714ff502f1ac9c
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2025-06-05 21:07:27 +0530

    LEETCODE #39

---
+++ LEETCODE EXTRAS/39.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+
+        def solve(curr, target, candidates, idx):
+            if target == 0:
+                result.append(curr)
+                return
+            
+            if target <= 0:
+                return
+            
+            for i in range(idx,len(candidates)):
+                solve(curr+[candidates[i]],target-candidates[i],candidates,i)
+            return
+
+
+        solve([], target , candidates, 0)
+
+        return result
+    
+    '''
+    Apprach: Similar to 296. Take it as reference.
+    '''
