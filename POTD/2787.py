1ffe209c6d970e2d19899781811c9f5e1ebd0232
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2025-08-12 19:51:58 +0530

    POTD 12 aug

--- POTD/2787.py
+++ POTD/2787.py
@@ -25,3 +25,49 @@ def numberOfWays(self, n: int, x: int) -> int:
 
         
         return solve(n,1)
+    
+'''
+    Java Code:
+    
+    class Solution {
+    static final int MOD = 1000000007;
+    int dp[][];
+    int X;
+
+    public int numberOfWays(int n, int x) {
+        this.X = x;
+        dp = new int[n+1][n+1];
+
+        for(int i = 0; i<= n ; i++){
+            for(int j = 0; j<= n; j++){
+                dp[i][j] = -1;
+            }
+        }
+
+        return solve(n,1);
+        
+    }
+
+    private int solve(int rem, int num){
+        if(rem == 0) return 1;
+        
+        if(rem<0) return 0;
+
+        if(Math.pow(num,X) > rem) return 0;
+
+        if(dp[rem][num] != -1) return dp[rem][num];
+
+        int take = solve(rem - (int) Math.pow(num,X),num+1);
+        int notTake = solve(rem, num+1);
+
+        dp[rem][num] = (int)(((long)take + notTake)%MOD);
+
+        return dp[rem][num];
+
+    }
+}
+    
+    
+    '''
+    
+ 
