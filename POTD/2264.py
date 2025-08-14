37eb5fbde34fc4cfe1bbda602bdc94e8e88c9410
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2025-08-14 21:40:00 +0530

    POTD 14 Aug

--- POTD/2264.py
+++ POTD/2264.py
@@ -9,3 +9,25 @@ def largestGoodInteger(self, num: str) -> str:
             return ""
 
         return maxstr * 3
+        
+'''
+Java Code:
+
+class Solution {
+    public String largestGoodInteger(String num) {
+        char maxstr = 0;
+
+        for(int i = 2; i < num.length(); i++){
+            if(num.charAt(i) == num.charAt(i-1) && num.charAt(i)==num.charAt(i-2)){
+                maxstr = (char) Math.max(maxstr, num.charAt(i));
+            }
+
+        }
+        if(maxstr == 0) return "";
+
+        return String.valueOf(maxstr).repeat(3);
+
+    }
+}
+
+'''
