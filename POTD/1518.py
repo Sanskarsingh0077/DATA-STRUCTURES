25c866055eeb255a893d387c7a4926140651e690
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2025-10-01 15:46:54 +0530

    POTD 1 Oct

---
+++ POTD/1518.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        empty = 0
+        ans = 0
+
+        while numBottles > 0:
+            ans += numBottles
+            empty += numBottles
+
+            numBottles = empty // numExchange
+            empty = empty % numExchange
+
+        return ans
