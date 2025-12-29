9ea455330269e43c92ecab4e493eaa0073dc570d
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2025-12-29 21:32:12 +0530

    .:

--- POTD/756.py
+++ POTD/756.py
@@ -1,10 +1,25 @@
 class Solution:
     def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
+        '''
+        # Default dict()
         mp = defaultdict(list)
 
         for pattern in allowed:
             mp[pattern[:2]].append(pattern[2]) # "ab -> c"
         print(mp)
+
+        '''
+        # Manual Map Creation
+        mp = {}
+
+        for pattern in allowed:
+            key = pattern[:2]
+            val = pattern[2]
+
+            if key not in mp:
+                mp[key] = []
+
+            mp[key].append(val)
 
         def solve(curr, mp, idx, above):
             if len(curr) == 1:
@@ -27,5 +42,7 @@ class Solution:
 
         
         return solve(bottom, mp, 0, "")
+
+        # TC : O( L ^ n )
 
 
