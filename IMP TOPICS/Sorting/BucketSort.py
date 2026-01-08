7f80141ccaa2195649b305d8b19a6e7215cb2498
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2026-01-08 14:30:28 +0530

    Bucket Sort

---
+++ IMP TOPICS/Sorting/BucketSort.py
@@ -0,0 +1,42 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        # Bucket Sort ---> O(n)
+
+        n = len(nums)
+
+        #Creating the freq Map
+        freq = {}
+
+        for num in nums:
+            if num in freq:
+                freq[num] += 1
+
+            else:
+                freq[num] = 1
+
+        #Create Buckets
+
+        buckets = [[] for _ in range(n+1)]
+
+        #fill buckets
+
+        for num, count in freq.items():
+            buckets[count].append(num)
+
+        res = []
+        #Accessing buckets and getting max freq nums == k
+
+        for f in range(len(buckets)- 1 , 0, -1):
+            for num in buckets[f]:
+                res.append(num)
+
+            if len(res) == k:
+                return res
+
+
+
+
+
+        
+
