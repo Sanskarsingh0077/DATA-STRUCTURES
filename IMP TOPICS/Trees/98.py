b5200991cd549828a1c56062a28a92d48510bc5a
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2026-02-05 19:31:08 +0530

    Binary Tree Validation #98

---
+++ IMP TOPICS/Trees/98.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def solve(root , left, right):
+            if not root:
+                return True
+                
+            if not (root.val < right and root.val > left):
+                return False
+            
+            # Check for both left and right 
+            return (solve(root.left, left, root.val) and 
+                    solve(root.right, root.val, right))
+
+
+        return solve(root, float('-inf'), float('inf'))
+
+
