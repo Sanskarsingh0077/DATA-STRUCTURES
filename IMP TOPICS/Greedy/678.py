9730ccf9d11de7d225cc0b204d29f923dc2988fc
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2026-02-27 20:07:50 +0530

    Greedy #678

---
+++ IMP TOPICS/Greedy/678.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        #Greedy Solution
+
+        leftmin = 0
+        leftmax = 0
+
+        for i in s:
+            if i == '(':
+                leftmin += 1
+                leftmax += 1
+
+            elif i == ')':
+                leftmin -= 1
+                leftmax -= 1
+
+            else:
+                # "*" Case
+                leftmin -= 1
+                leftmax += 1
+
+            if leftmax < 0:
+                return False
+
+            if leftmin < 0:
+                leftmin = 0
+
+        return leftmin == 0
+                
+
+
