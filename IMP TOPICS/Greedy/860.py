9697900ba0e78a14dc47d4bc307b2963fed015b0
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2026-03-06 20:27:51 +0530

    Greedy #860

---
+++ IMP TOPICS/Greedy/860.py
@@ -0,0 +1,26 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        fives = 0
+        tens = 0
+
+        for i in range(len(bills)):
+            if bills[i] == 5:
+                fives += 1
+
+            elif bills[i] == 10:
+                if fives == 0:
+                    return False
+                tens += 1
+                fives -= 1
+
+            elif bills[i] == 20:
+                if tens > 0 and fives > 0:
+                    tens -= 1
+                    fives -= 1
+                elif fives >= 3:
+                    fives -= 3
+                else:
+                    return False
+
+
+        return True
