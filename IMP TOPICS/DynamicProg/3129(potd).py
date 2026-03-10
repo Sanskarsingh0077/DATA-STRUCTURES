c1b57e9e651950c862377389958c8cc9de0a477e
Sanskarsingh0077 <sanskarsingh5129@gmail.com>
2026-03-10 19:54:59 +0530

    POTD

--- IMP TOPICS/DynamicProg/3129(potd).py
+++ IMP TOPICS/DynamicProg/3129(potd).py
@@ -58,3 +58,39 @@ class Solution:
 
         return (t[one][zero][0] + t[one][zero][1])%mod
 
+# 3130 --- 1000 size input
+
+class Solution:
+    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
+        mod = 10**9 + 7
+
+        t = [[[0]*2 for _ in range(one+1)] for _ in range(zero+1)]
+
+        #t[zero][one][usedlast(0/1)]
+
+        for i in range(1,min(limit, zero)+1):
+            t[i][0][0] = 1
+
+        for j in range(1,min(limit, one)+1):
+            t[0][j][1] = 1
+
+        
+        for i in range(zero+1):
+            for j in range(one+1):
+                
+                if i == 0 or j == 0:
+                    continue
+
+                    
+                t[i][j][1] = (t[i][j-1][0] + t[i][j-1][1]) % mod 
+
+                if j - 1 >= limit:
+                    t[i][j][1] = (t[i][j][1] - t[i][j-1-limit][0])%mod
+
+            
+                t[i][j][0] = (t[i-1][j][0] + t[i-1][j][1])%mod 
+
+                if i -1 >= limit:
+                    t[i][j][0] = (t[i][j][0] - t[i-1-limit][j][1])%mod
+
+        return (t[zero][one][0] + t[zero][one][1]) % mod
